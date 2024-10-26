1c57b31752c2bf3bdc8a0f093ca286e861000d77
Franklin Poko <franklin.poko@firstbanknigeria.com>
2024-10-26 12:22:41 +0100

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import staticPlugin from 'vite-plugin-static';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    staticPlugin({
+      base: '/repository-name/',  // Replace with your repository name
+    }),
+  ],
+});
