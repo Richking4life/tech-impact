3adff41b50cf1cc235fbb5685499e6dd57632296
Franklin Poko <franklin.poko@firstbanknigeria.com>
2024-10-26 12:33:21 +0100

    rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import staticPlugin from 'vite-plugin-static';
 
 export default defineConfig({
-  base: '/tech-impact/',  // Replace 'repository-name' with your actual repository name
+  base: '/tech-blog/',  // Replace 'repository-name' with your actual repository name
   plugins: [
     react(),
     staticPlugin(),
